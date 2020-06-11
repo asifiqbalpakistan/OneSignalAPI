b08783b1304065ffeaa54108690e9d8c82502219
asifiqbalpakistan <m_asifiqbal@outlook.com>
2020-06-11 22:16:33 +0400

    first commit

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace AG_OneSignalAPI
+{
+    public static class WebApiConfig
+    {
+        //public const string URL_APPS = "https://onesignal.com/api/v1/apps"; // DO NOT TOUCH THIS 
+        
+        //public const string API_KEY = "MDM4MzI4ZTMtYjI4ZS00YzA0LThiZDYtNGE1YTMxZDc3MjQ0";
+
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Models/OneSignalDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AG_OneSignalAPI.Models
+{
+    public class OneSignalDetails
+    {
+        [DisplayName("APP ID")]
+        public string id { get; set; }
+        [Required]
+        [DisplayName("Name")]
+        public string name { get; set; }
+        [DisplayName("Site URL")]
+        [Required]
+        public string chrome_web_origin { get; set; }
+        [DisplayName("Created Date")]
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy HH:mm}")]
+        public DateTime created_at { get; set; }
+        
+        public DateTime updated_at { get; set; }
+        [DisplayName("APP Key")]
+        public string basic_auth_key { get; set; }
+
+    }
+}
